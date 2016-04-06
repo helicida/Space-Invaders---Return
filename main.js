0820e6ec8903d4532cac9d45fd5662b440da8bd0
Sergi Barjola <sergibca@gmail.com>
2016-04-06 19:47:58 +0200

    Commit inicial, inicio del proyecto

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 /// <reference path="phaser/phaser.d.ts"/>
 var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+        function __() {
+            this.constructor = d;
+        }
+
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
 var Point = Phaser.Point;
 var mainState = (function (_super) {
     __extends(mainState, _super);
     function mainState() {
         _super.apply(this, arguments);
     }
+
     mainState.prototype.preload = function () {
         _super.prototype.preload.call(this);
     };
@@ -26,6 +30,7 @@ var Ladrillo = (function (_super) {
     function Ladrillo(game, x, y, key, frame) {
         _super.call(this, game, x, y, key, frame);
     }
+
     return Ladrillo;
 })(Phaser.Sprite);
 var SimpleGame = (function () {
@@ -34,6 +39,7 @@ var SimpleGame = (function () {
         this.game.state.add('main', mainState);
         this.game.state.start('main');
     }
+
     return SimpleGame;
 })();
 window.onload = function () {
